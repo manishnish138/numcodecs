9537ee5fc89c825f3720ddfc810a338a77daeb5d
manishnish138 <82148691+manishnish138@users.noreply.github.com>
2021-08-04 11:18:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,10 +31,10 @@ if have_cflags:
 elif os.name == 'posix':
     if disable_sse2:
         base_compile_args.append('-mno-sse2')
-    elif have_sse2:
-        base_compile_args.append('-msse2')
-    if disable_avx2:
-        base_compile_args.append('-mno-avx2')
+    #elif have_sse2:
+    #    base_compile_args.append('-msse2')
+    #if disable_avx2:
+    #    base_compile_args.append('-mno-avx2')
     elif have_avx2:
         base_compile_args.append('-mavx2')
 # On macOS, force libc++ in case the system tries to use `stdlibc++`.
